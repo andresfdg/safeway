223baf7a66911023154fca75f88c0196d67e9921
Andres <inciapipe@live.com>
2022-07-09 13:36:05 -0400

    done

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <Navbar />
+  <div class="app">
+    <router-view />
+  </div>
+</template>
+
+<style>
+.app {
+  text-align: center;
+}
+</style>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+export default {
+  components: { Navbar },
+};
+</script>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <Calcu />
+  </div>
+</template>
+<style scoped>
+.about {
+  background-color: cornflowerblue;
+  min-height: 800px;
+}
+</style>
+
+<script>
+import Calcu from "@/components/Calcu.vue";
+export default {
+  components: { Calcu },
+};
+</script>
